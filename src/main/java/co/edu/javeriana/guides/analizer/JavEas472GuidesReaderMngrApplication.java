f8adc663a390d047c3595dac88eb515544f2b489
ingAndresMartinez <57421413+ingAndresMartinez@users.noreply.github.com>
2020-09-01 20:50:26 -0500

    Primer commit - Proyecto maven

---
+++ src/main/java/co/edu/javeriana/guides/analizer/JavEas472GuidesReaderMngrApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.guides.analizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEas472GuidesReaderMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEas472GuidesReaderMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/guides/analizer/JavEas472GuidesReaderMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.guides.analizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavEas472GuidesReaderMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
